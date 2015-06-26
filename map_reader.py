2878d663d66707173dca0a095a9631daeae76ca4
Nicolas Grimaud <ketchu13@hotmail.com>
2015-06-26 18:32:56 +0200

    11.6 fix

--- map_reader.py
+++ map_reader.py
@@ -26,6 +26,10 @@ import os
 import time
 import sqlite3
 
+__version__ = "1.3.3-dev"
+
+print "Welcome to 7DTD leflet builder version " + __version__
+
 try:
     from PIL import Image, ImageOps
 except ImportError:
@@ -106,16 +110,26 @@ class MapReader:
             if not curs.read(4) == "map\0":
                 print "Skip "+os.path.basename(map_file)+" wrong file header"
                 return
-            curs.read(1)
+            ## Read version
+            version = struct.unpack("I", curs.read(4))[0]
+
+            tiles_pos = 524297
+            if version == 2:
+                tiles_pos = 524300
+            else:
+                print "Warning old map version: ", version
+                curs.seek(5)
+
             #######################
             # read index
             num = struct.unpack("I", curs.read(4))[0]
+
             # read tiles position
             tiles_index = [struct.unpack("i", curs.read(4))[0] for i in xrange(num)]
             #######################
             # read tiles pixels
             if not index_only:
-                curs.seek(524297)
+                curs.seek(tiles_pos)
                 for i in xrange(num):
                     if self.store_history or not self.is_tile_stored(tiles_index[i]):
                         # extract 16-bytes pixel 16*16 tile
