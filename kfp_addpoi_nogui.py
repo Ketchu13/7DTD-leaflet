4f4c24b9a543041cd4814592fd84838f46c1305b
Michal Palma <michal.palma@gmail.com>
2015-08-14 21:19:08 +0200

    fixed POI location

--- kfp_addpoi_nogui.py
+++ kfp_addpoi_nogui.py
@@ -225,8 +225,8 @@ class KFP_AddPOI(threading.Thread):
                                      sid = s[l + 8:s.find(',', l)]
                                      locTp = s[j + 7:]
                                      loc = locTp[:locTp.find('), rot')]#.split(', ')
-                                     locx = int(float(loc.split(', ')[0]))
-                                     locy = int(float(loc.split(', ')[2]))
+                                     locy = int(float(loc.split(', ')[0]))
+                                     locx = int(float(loc.split(', ')[2]))
                                      if self.parent.parent.settings['ignTrack']:
                                         tracks = [(psd , locx, locy)]
                                         self.th_tracks = self.parent.updateTracks_csv(self,tracks)
