3b8dd290f6aa6590b13615bd5109f11f8e6473a1
Nicolas Grimaud <ketchu13@hotmail.com>
2015-08-16 02:22:52 +0200

    Revert "Update to the last mapreader version"
    This reverts commit 8f414ac0ae37a20e6e556aa73a46712b04851d53.

--- map_reader.py
+++ map_reader.py
@@ -206,16 +206,7 @@ class Advanced_MapReader(threading.Thread):
                 if not curs.read(4) == "map\0":
                     print "Skip "+os.path.basename(map_file)+" wrong file header"
                     return
-                ## Read version
-                version = struct.unpack("I", curs.read(4))[0]
-
-                tiles_pos = 524297
-                if version == 2:
-                    tiles_pos = 524300
-                else:
-                    print "Warning old map version: ", version
-                    curs.seek(5)
-
+                curs.read(1)
                 #######################
                 # read index
                 num = struct.unpack("I", curs.read(4))[0]
@@ -224,7 +215,7 @@ class Advanced_MapReader(threading.Thread):
                 #######################
                 # read tiles pixels
                 if not index_only:
-                    curs.seek(524300)
+                    curs.seek(524297)
                     for i in xrange(num):
                         if self.store_history or not self.is_tile_stored(tiles_index[i]):
                             # extract 16-bytes pixel 16*16 tile
