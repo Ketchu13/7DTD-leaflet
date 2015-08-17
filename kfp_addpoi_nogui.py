c4fbb133348e01a9bb7a2c480a6e9a2fb94ac8d3
Nicolas G <ketchu13@hotmail.com>
2015-08-17 10:50:03 +0200

    Fix: /addpoi,  erases previous data

--- kfp_addpoi_nogui.py
+++ kfp_addpoi_nogui.py
@@ -115,7 +115,7 @@ class KFP_AddPOI(threading.Thread):
         def writePoi(self, x, psdR, sid, poiName, loc):
              try:
                  old = self.readPoi(x)
-                 with open(x, "w") as f:
+                 with open(x, "r+") as f:
                      f.write(old + '\n<poi sname=\"' + psdR + '\" steamId=\"' + sid + '\" pname=\"' + poiName + '\" pos=\"' + loc + '\" icon=\"farm\" />\n</poilist>')
                  old = self.readPoi(x)   
                  return True
