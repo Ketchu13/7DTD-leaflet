5b8140989ffd94373e77e0315b503ff3719c16b2
Nicolas Grimaud <ketchu13@hotmail.com>
2015-08-22 19:12:14 +0200

    Fix and clean

--- simple_server.py
+++ simple_server.py
@@ -1,17 +1,18 @@
+# coding=utf-8
 import sys
 import BaseHTTPServer
 from SimpleHTTPServer import SimpleHTTPRequestHandler
 
 
 HandlerClass = SimpleHTTPRequestHandler
-ServerClass  = BaseHTTPServer.HTTPServer
-Protocol     = "HTTP/1.0"
+ServerClass = BaseHTTPServer.HTTPServer
+Protocol = "HTTP/1.0"
 
 if sys.argv[1:]:
     port = int(sys.argv[1])
 else:
-    port = 8081
-server_address = ('127.0.0.1', port)
+    port = 8082
+server_address = ('', port)
 
 HandlerClass.protocol_version = Protocol
 httpd = ServerClass(server_address, HandlerClass)
