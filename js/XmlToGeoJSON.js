7f7201898dc85dd00a74c3c2e099b467ba36185e
Nicolas Grimaud <ketchu13@hotmail.com>
2015-08-22 18:16:42 +0200

    Add New icons in js script

--- js/XmlToGeoJSON.js
+++ js/XmlToGeoJSON.js
@@ -17,45 +17,45 @@ function escapeHTML(str){
 }
 
 function ShowPOILocation() {
-	var data =[];
-	if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
-			xmlhttp=new XMLHttpRequest();
-	} else {// code for IE6, IE5
-			xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-	}
-	xmlhttp.open("GET", "./xml/POIList.xml", false);
-	xmlhttp.send();
-	xmlDoc=xmlhttp.responseXML;
-	var pois = xmlDoc.getElementsByTagName("poilist")[0].getElementsByTagName("poi");
-		if(! pois.length == 0 ) {                   
-			try {			
-				for (var j = 0; j < pois.length; j++) {      
-					var Id = pois[j].getAttribute("id");
-					var steamId = pois[j].getAttribute("steamId");
-					var lpblock = pois[j].getAttribute("pos").split(",");
-					var pname =  pois[j].getAttribute("pname");
-					var sname =  pois[j].getAttribute("sname");
-					var icone =  pois[j].getAttribute("icon");
-					var name = pois[j].getAttribute("name");
-						var ct = escapeHTML(pname) + "<br>Signaled by " + escapeHTML(sname);
-						var prop0 = {
-							"icon": window[icone],
-							"popupContent": ct,
-							"entity": 1,
-							"hidden": false
-						};
-						var data3 = {
-							"type": "Feature",
-							"geometry": {
-								"type": "Point",
-								"coordinates": [lpblock[1], lpblock[0]]
-							},						
-							"properties": prop0
-						};							
-						 
-						data.push(data3);
-				}
-			} catch(ex){alert(ex);}
-		} 
-	return [{"type": "FeatureCollection", "features": data }];
+    var data =[];
+    if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
+            xmlhttp=new XMLHttpRequest();
+    } else {// code for IE6, IE5
+            xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.open("GET", "./xml/POIList.xml", false);
+    xmlhttp.send();
+    xmlDoc=xmlhttp.responseXML;
+    var pois = xmlDoc.getElementsByTagName("poilist")[0].getElementsByTagName("poi");
+        if(! pois.length == 0 ) {
+            try {
+                for (var j = 0; j < pois.length; j++) {
+                    var Id = pois[j].getAttribute("id");
+                    var steamId = pois[j].getAttribute("steamId");
+                    var lpblock = pois[j].getAttribute("pos").split(",");
+                    var pname =  pois[j].getAttribute("pname");
+                    var sname =  pois[j].getAttribute("sname");
+                    var icone =  pois[j].getAttribute("icon");
+                    var name = pois[j].getAttribute("name");
+                        var ct = escapeHTML(pname) + "<br>Signaled by " + escapeHTML(sname);
+                        var prop0 = {
+                            "icon": window[icone],
+                            "popupContent": ct,
+                            "entity": 1,
+                            "hidden": false
+                        };
+                        var data3 = {
+                            "type": "Feature",
+                            "geometry": {
+                                "type": "Point",
+                                "coordinates": [lpblock[1], lpblock[0]]
+                            },
+                            "properties": prop0
+                        };
+
+                        data.push(data3);
+                }
+            } catch(ex){alert(ex);}
+        }
+    return [{"type": "FeatureCollection", "features": data }];
 }
